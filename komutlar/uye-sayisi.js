38d160823a43cdf863f5707b8f0e4cb3098b73c9
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-02-26 22:32:24 +0300

    Update uye-sayisi.js

--- komutlar/uye-sayisi.js
+++ komutlar/uye-sayisi.js
@@ -3,21 +3,21 @@ module.exports.run = async (bot, message, args) => {
     let üyesayi = message.guild.memberCount;
     let botlar = message.guild.members.filter(m => m.user.bot).size;
     let kullanıcılar = üyesayi - botlar;
-    let tag = 'ꖜ'
+    //let tag = 'ꖜ'
 const embed = new Discord.RichEmbed()
 .setColor(`#CE2D0B`)
 .setTimestamp()
 .addField(`Toplam Üye`, `**${üyesayi}**`, true)
 .addField(`Kullanıcılar`, `**${kullanıcılar}**`, true)
 .addField(`Botlar`, `**${botlar}**`, true)
-.addField("Tagdaki üye sayısı :", message.guild.members.filter(m => m.user.username.includes(tag)).size)
+//.addField("Tagdaki üye sayısı :", message.guild.members.filter(m => m.user.username.includes(tag)).size)
 .addField(`Üye Durumları`, `**${message.guild.members.filter(o => o.presence.status === 'online').size}** Çevrimiçi\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** Boşta\n**${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size}** Rahatsız Etmeyin\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Çevrimdışı/Görünmez`, true)
 message.channel.send(embed)
 }
 module.exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["üyedurum","üyeler","durumlar","durum"],
+  aliases: ["sayı"],
   permLevel: 0
 };
 
