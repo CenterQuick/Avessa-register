aecf257fafe9e1e8805f48168b5378180bbdef01
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-02-26 22:17:38 +0300

    Update uye-sayisi.js

--- komutlar/uye-sayisi.js
+++ komutlar/uye-sayisi.js
@@ -3,16 +3,12 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
     if (!message.guild) return message.author.sendMessage('Bu Komutu Sadece Sunucularda Kulanabilirsiniz!');
 
-    const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
-    let count = 0;
-    for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
     let tag = 'ꖜ'
     const sayem = new Discord.RichEmbed()
         .setColor("RED")
     .setTitle(`Sunucu Durumu`)
         .addField("Sunucudaki üye sayısı :", message.guild.memberCount)
         .addField("Çevrimiçi üye sayısı :", message.guild.members.filter(m => !m.user.bot && m.user.presence.status !== "offline").size)
-        .addField("Seslideki üye sayısı :", count)
         .addField("Tagdaki üye sayısı :", message.guild.members.filter(m => m.user.username.includes(tag)).size)
     message.channel.send(sayem);
 
