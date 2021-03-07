8c2a686ad8617c279103af44fc5e3c96abe8f1c9
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2021-03-07 14:50:28 +0300

    Update kayıt-erkek.js

--- komutlar/kayıt-erkek.js
+++ komutlar/kayıt-erkek.js
@@ -3,19 +3,19 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
   
-if(!["803679504479420476"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
+if(!["818078142421008434"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
   
-const erkek = message.guild.roles.cache.find(r => r.id === "803679499488067596")
-const kayıtsız = message.guild.roles.cache.find(r => r.id === "803679506837143602")
-const reglog = message.guild.channels.cache.find(c => c.id === "803679527526989824")
-const genelchat = message.guild.channels.cache.find(g => g.id === "803679538756190229")
+const erkek = message.guild.roles.cache.find(r => r.id === "818073478459555850")
+const kayıtsız = message.guild.roles.cache.find(r => r.id === "818073869985906708")
+const reglog = message.guild.channels.cache.find(c => c.id === "818079254264676374")
+const genelchat = message.guild.channels.cache.find(g => g.id === "818080579407970306")
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
 if(!member) return message.channel.send(`Bir Kullanıcı Belirt.`)
 if(!member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(`Bu Kullanıcı Sizle Üst/Aynı Pozisyondadır.`)
 const x = message.guild.member(member)
 
-let tag = "¹³"
+let tag = "ʡ"
 let isim = args[1]
 let yas = Number(args[2])
 if(!isim) return message.channel.send(`Bir İsim Belirt`)
@@ -27,11 +27,11 @@ db.add(`yetkili.${message.author.id}.toplam`, 1)
 let toplami = db.fetch(`yetkili.${message.author.id}.toplam`)  
 
 message.react('✅')
-x.setNickname(`${isim} ${tag} • ${yas}`)
+x.setNickname(`${tag} ${isim} • ${yas}`)
 x.roles.add(erkek)
 x.roles.remove(kayıtsız)
 //
-x.setNickname(`${isim} ${tag} • ${yas}`)
+x.setNickname(`${tag} ${isim} • ${yas}`)
 x.roles.add(erkek)
 x.roles.remove(kayıtsız)
 
@@ -53,7 +53,7 @@ const embed = new Discord.MessageEmbed()
     .setThumbnail(message.author.avatarURL())
 .setFooter(`Heartling Register`)
 .setColor('RED')
-client.channels.cache.get('803679538756190229').send(embed)
+client.channels.cache.get('818080579407970306').send(embed)
 
 genelchat.send(`<@${member.id}>, Aramıza Hoş Geldin ! İyi eğlenceler`)
 
